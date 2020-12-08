e3e7c8c23a76bc56d0b0f3784f14686de4881499
DominikWolek <dominik.artur.wolek@gmail.com>
2020-12-08 07:51:37 +0100

    Use Data.Array

--- day_08/8.hs
+++ day_08/8.hs
@@ -2,68 +2,66 @@ import Text.Printf
 import qualified Data.Set as S
 import Data.Either
 import Control.Monad.State
+import Data.Array
 
 inputPath = "input"
 
 type Instruction = (String, Int)
+type Instructions = Array Int Instruction
 type OpState = (Int, Int)
+-- Left acc: infinite loop
+-- Right acc: finished
 type ExecReturn = (Either Int Int)
 type ExecState = (S.Set Int, OpState)
 
-fromLeft :: Either a b -> a
-fromLeft = either id (error "Right")
-
-fromRight :: Either a b -> b
-fromRight = either (error "Left") id
-
-first :: [Instruction] -> Int
-first input = fromLeft $ evalState (exec input) (S.empty, (0, 0))
-
-exec :: [Instruction] -> State ExecState ExecReturn
+exec :: Instructions -> State ExecState ExecReturn
 exec instructions = do
-    (set, (acc, ptr)) <- get
-    if ptr >= length instructions
+    (set, (acc, i)) <- get
+    if i >= length instructions
         then return $ Right acc
-        else if S.member ptr set
+        else if S.member i set
             then return $ Left acc
             else do
-                execOne (instructions !! ptr)
+                execOne (instructions ! i)
                 exec instructions
 
 execOne :: Instruction -> State ExecState ()
 execOne instruction = do
-    (set, (acc, ptr)) <- get
-    let opState = interpret instruction (acc, ptr)
-    let newSet = S.insert ptr set
+    (set, (acc, i)) <- get
+    let opState = interpret instruction (acc, i)
+    let newSet = S.insert i set
     put (newSet, opState)
+
+interpret :: Instruction -> OpState -> OpState
+interpret ("acc", n) (acc, i) = (acc + n, i + 1)
+interpret ("jmp", n) (acc, i) = (acc, i + n)
+interpret ("nop", _) (acc, i) = (acc, i + 1)
+
+first :: Instructions -> Int
+first instructions = head.lefts $ [evalState (exec instructions) (S.empty, (0, 0))]
 
 change :: Instruction -> Instruction
 change ("acc", n) = ("acc", n)
 change ("jmp", n) = ("nop", n)
 change ("nop", n) = ("jmp", n)
 
-second :: [Instruction] -> Int -> Int
-second input n =
-    if (fst instruction == "acc") || continue
-        then second input (n + 1)
-        else fromRight calc
+second :: Instructions -> Int
+second instructions = head.rights.map (changeN instructions) $ [0..]
+
+changeN :: Instructions -> Int -> ExecReturn
+changeN instructions n =
+    evalState (exec newInstructions) (S.empty, (0,0))
     where
-        instruction = change (input !! n)
-        newInstructions = take n input ++ (instruction: drop (n + 1) input)
-        calc = evalState (exec newInstructions) (S.empty, (0,0))
-        continue = isLeft calc
+        instruction = change (instructions ! n)
+        newInstructions = instructions // [(n, instruction)]
 
 parse :: String -> Instruction
 parse (i1:i2:i3:_:'+':n) = (i1:i2:[i3], read n::Int)
 parse (i1:i2:i3:_:'-':n) = (i1:i2:[i3], read ('-':n)::Int)
 
-interpret :: Instruction -> OpState -> OpState
-interpret ("acc", n) (acc, ptr) = (acc + n, ptr + 1)
-interpret ("jmp", n) (acc, ptr) = (acc, ptr + n)
-interpret ("nop", _) (acc, ptr) = (acc, ptr + 1)
-
 main :: IO ()
 main = do
     input <- map parse.lines <$> readFile inputPath
-    printf "Silver star:\t%d\n" $ first input
-    printf "Gold star:  \t%d\n" $ second input 0
+    let instructions = listArray (0, (length input) - 1) input
+    printf "Silver star:\t%d\n" $ first instructions
+    printf "Gold star:  \t%d\n" $ second instructions
